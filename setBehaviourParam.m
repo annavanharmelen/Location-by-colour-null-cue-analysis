24fc0a26770734d046f472e0d22a745d2d6237e4
Anna work laptop <a.m.van.harmelen@vu.nl>
2024-03-14 11:59:10 +0100

    set behaviour parameters in separate function

---
+++ setBehaviourParam.m
@@ -0,0 +1,49 @@
+function [bar_size, colours, dark_colours, labels, subplot_size, percentageok] = setBehaviourParam(pp2do)
+%SETBEHAVIOURPARAM sets the parameters for analysing the behavioural data.
+bar_size = 0.8;
+
+colours = [114, 182, 161;...
+           149, 163, 192;...
+           233, 150, 117;...
+           194, 102, 162];
+colours = colours/255;
+
+dark_colours = [50, 79, 70;
+                58, 67, 88;
+                105, 67, 52;
+                92, 49, 77];
+dark_colours = dark_colours/255;
+
+% labels = {'congruent', 'incongruent', 'congruent', 'incongruent'};
+labels = {'location cue', 'colour cue'};
+
+subplot_size = ceil(sqrt(size(pp2do, 2)));
+
+percentageok = zeros(size(pp2do));
+precision = zeros(size(pp2do, 2), 2);
+pT = zeros(size(pp2do, 2), 2);
+pNT = zeros(size(pp2do, 2), 2);
+pU = zeros(size(pp2do, 2), 2);
+decisiontime = zeros(size(pp2do, 2), 4);
+decisiontime_std = zeros(size(pp2do, 2), 4);
+error = zeros(size(pp2do, 2), 4);
+overall_dt = zeros(size(pp2do));
+overall_error = zeros(size(pp2do));
+capture_rt = zeros(size(pp2do, 2), 3);
+capture_error = zeros(size(pp2do, 2), 3);
+capture_effectsize = zeros(size(pp2do, 2), 3);
+ppnum = zeros(size(pp2do));
+total_correct = zeros(size(pp2do, 2), 4);
+total_wrong = zeros(size(pp2do, 2), 4);
+total_correct_capture = zeros(size(pp2do, 2), 4);
+proportion_correct = zeros(size(pp2do, 2), 4);
+proportion_wrong = zeros(size(pp2do, 2), 4);
+proportion_correct_capture = zeros(size(pp2do, 2), 4);
+
+
+overall_dt = zeros(size(pp2do));
+overall_error = zeros(size(pp2do));
+
+
+end
+
