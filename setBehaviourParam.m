553f7ff9da9c418de71cce1f59892e0fdc9bea8f
Anna work laptop <a.m.van.harmelen@vu.nl>
2024-09-26 17:26:15 +0200

    update colours

--- setBehaviourParam.m
+++ setBehaviourParam.m
@@ -2,19 +2,10 @@ function [bar_size, colours, dark_colours, labels, subplot_size, percentageok] =
 %SETBEHAVIOURPARAM sets the parameters for analysing the behavioural data.
 bar_size = 0.8;
 
-colours = [114, 182, 161;...
-           149, 163, 192;...
-           233, 150, 117;...
-           194, 102, 162];
-colours = colours/255;
+colours = [hex2rgb("#3E8DD6"); hex2rgb("#D649C3")];
 
-dark_colours = [50, 79, 70;
-                58, 67, 88;
-                105, 67, 52;
-                92, 49, 77];
-dark_colours = dark_colours/255;
+dark_colours = [hex2rgb("#143756"); hex2rgb("#611F59"); hex2rgb("#143756"); hex2rgb("#611F59")];
 
-% labels = {'congruent', 'incongruent', 'congruent', 'incongruent'};
 labels = {'location cue', 'colour cue'};
 
 subplot_size = ceil(sqrt(size(pp2do, 2)));
