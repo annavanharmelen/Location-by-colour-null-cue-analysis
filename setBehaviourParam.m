96a965b5c4338a0c13417fa4d3bafd0d3d14fbfb
Anna work laptop <a.m.van.harmelen@vu.nl>
2024-09-27 15:49:34 +0200

    fix colour schemes

--- setBehaviourParam.m
+++ setBehaviourParam.m
@@ -2,9 +2,10 @@ function [bar_size, colours, dark_colours, labels, subplot_size, percentageok] =
 %SETBEHAVIOURPARAM sets the parameters for analysing the behavioural data.
 bar_size = 0.8;
 
-colours = [hex2rgb("#3E8DD6"); hex2rgb("#D649C3")];
+colours = [hex2rgb("#3E8DD6"); hex2rgb("#D649C3"); hex2rgb("#27A579")];
 
-dark_colours = [hex2rgb("#143756"); hex2rgb("#611F59"); hex2rgb("#143756"); hex2rgb("#611F59")];
+dark_colours = [hex2rgb("#143756"); hex2rgb("#611F59"); hex2rgb("#004029")];
+dark_colours = [dark_colours;dark_colours];
 
 labels = {'location cue', 'colour cue'};
 
